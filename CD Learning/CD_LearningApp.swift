c43c47d3c299dcbbb8acd5c61003734ee91a0af8
Arunkumar Chandrasekar <arunkumar.c@innoppl.com>
2025-05-06 12:28:43 +0530

    Initial Commit

---
+++ CD Learning/CD_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  CD_LearningApp.swift
+//  CD Learning
+//
+//  Created by Arun on 06/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CD_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CD LearningTests/CD_LearningTests.swift
@@ -0,0 +1,17 @@
+//
+//  CD_LearningTests.swift
+//  CD LearningTests
+//
+//  Created by Arun on 06/05/25.
+//
+
+import Testing
+@testable import CI_CD_Learning
+
+struct CD_LearningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
